6efa4fcf5e95e062bae3d55ca4a45513e010b93f
ldoc <leandrodelolmo@gmail.com>
2026-08-27 19:14:30 +0200

    feat: add PDF generator (orchestrates all sections)

---
+++ api/pdf/generator.js
@@ -0,0 +1,39 @@
+import PDFDocument from 'pdfkit';
+import { PAGE_WIDTH, PAGE_HEIGHT, MARGIN, drawPlayerHeader } from './layout.js';
+import { generateCover } from './cover.js';
+import { drawPredictions } from './predictions.js';
+import { drawBracket } from './bracket.js';
+import { drawPlantilla } from './plantilla.js';
+
+export function generatePDF(users, matchesByRound, teamsMap) {
+  return new Promise((resolve, reject) => {
+    const doc = new PDFDocument({
+      size: 'A4',
+      margins: { top: MARGIN, bottom: MARGIN, left: MARGIN, right: MARGIN },
+      autoFirstPage: false,
+    });
+
+    const chunks = [];
+    doc.on('data', chunk => chunks.push(chunk));
+    doc.on('end', () => resolve(Buffer.concat(chunks)));
+    doc.on('error', reject);
+
+    doc.addPage();
+    generateCover(doc, users.length);
+
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      console.log(`Generando página ${i + 1}/${users.length}: ${user.username}`);
+
+      doc.addPage();
+      let y = MARGIN;
+
+      y = drawPlayerHeader(doc, user, y);
+      y = drawPredictions(doc, matchesByRound, user.predictions || {}, teamsMap, y);
+      y = drawBracket(doc, user.finalPredictions, teamsMap, y);
+      y = drawPlantilla(doc, user.squad, y);
+    }
+
+    doc.end();
+  });
+}
